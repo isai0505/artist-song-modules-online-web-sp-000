452d2805f727d059fefa96b21ae22dbd19b554b7
Isai Bismark Cortez <isai0505@gmail.com>
2019-09-24 12:26:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,24 @@
 require 'pry'
-
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
 class Artist
+  extend Memorable
+  include Paramable
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
+  # def initialize
+  #   @@artists << self
+  #   @songs = []
+  # end
+
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
+end
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
@@ -17,14 +26,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -36,7 +37,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,21 @@
 require 'pry'
-
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
 class Song
+  extend Memorable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
+  # def initialize
+  #   @@songs << self
+  # end
+
   def initialize
-    @@songs << self
+    super
+    @artist = []
   end
 
   def self.find_by_name(name)
@@ -18,19 +26,11 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end  
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+end
